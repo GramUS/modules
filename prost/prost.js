ca0a0c9be8a30acaeae6a1ed4628be773d4b265b
Roj Serbest <49933115+rojserbest@users.noreply.github.com>
2021-06-28 12:44:29 +0300

    Delete prost.js

--- prost/prost.js
+++
@@ -1,5 +0,0 @@
-const { CommandHandler, editMessageToResult } = require("../dist/userbot");
-
-module.exports = new CommandHandler("prost", async function (event) {
-    await editMessageToResult(event, "Stone ist prost!");
-});
