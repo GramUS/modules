1552d45d4438c8362335b003ea48bf0d8a771408
Roj Serbest <49933115+rojserbest@users.noreply.github.com>
2021-06-28 12:44:20 +0300

    Delete error.js

--- error/error.js
+++
@@ -1,5 +0,0 @@
-const { CommandHandler, editMessageToError } = require("../dist/userbot");
-
-module.exports = new CommandHandler("error", async function (event) {
-    await editMessageToError(event, "Wrr!");
-});
